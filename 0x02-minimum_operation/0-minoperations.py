b780ae3f6a7c8caf268647c55b17ae6fd1c2ca14
Saviour-Dagadu <savicane@gmail.com>
2024-07-13 06:05:20 +0000

    Adding chnages made to 0x02-minimum_operation

--- 0x02-minimum_operation/0-minoperations.py
+++ 0x02-minimum_operation/0-minoperations.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Min Operations"""
+""" Minimum Operations
+    """
 
-def minOperations(n):
-    if n == 1:
+
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
         return 0
-    
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-    
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + (i // j))
-    
-    return dp[n] if dp[n] != float('inf') else 0
+    return op
