86a86dfeb319be2f5f1493a8a8d440dc85244d16
Saviour-Dagadu <savicane@gmail.com>
2024-07-13 05:54:55 +0000

    Adding chnages made to 0x02-minimum_operation

--- 0x02-minimum_operation/0-minoperations.py
+++ 0x02-minimum_operation/0-minoperations.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
 def minOperations(n):
     if n == 1:
         return 0
-    operations = 0
-    while n > 1:
-        if n % 2 == 0:
-            n = n // 2
-        else:
-            n = n + 1
-        operations += 1
-    return operations + 1
+    
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+    
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + (i // j))
+    
+    return dp[n] if dp[n] != float('inf') else 0
